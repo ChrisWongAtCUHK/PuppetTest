0571b881dc96be6abec6daa53b32e24e1e875440
chriswongatcuhk <wc38.wong@gmail.com>
2019-11-02 16:49:02 +0800

    Simple demo

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using PuppeteerSharp;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PuppetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test().Wait();
+        }
+
+        static async Task Test()
+        {
+            try
+            {
+                await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+                using (var browser = await Puppeteer.LaunchAsync(new LaunchOptions()
+                {
+                    Headless = false,
+                    SlowMo = 1000 // if it is 100, program works
+                }))
+                {
+                    using (var page = await browser.NewPageAsync())
+                    {
+                        const string siteName = "duckduckgo";
+                        await page.GoToAsync($"https://{siteName}.com/");
+                        await page.ScreenshotAsync($"{siteName}.png");
+                        await page.CloseAsync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+[SlowMo prevents the page from loading #716](https://github.com/kblok/puppeteer-sharp/issues/716)
